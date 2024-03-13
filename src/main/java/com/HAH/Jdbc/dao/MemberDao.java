09c6cf04fc15e5fd8c0ad7015bd1d1c94eb323ef
hhtun137 <htunaunghlaing199@gmail.com>
2024-03-13 12:45:32 +0630

    annotation config for Jdbc

--- src/main/java/com/HAH/Jdbc/dao/MemberDao.java
+++ src/main/java/com/HAH/Jdbc/dao/MemberDao.java
@@ -1,17 +1,16 @@
 package com.HAH.Jdbc.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.HAH.Jdbc.dto.Member;
 
+@Repository
 public class MemberDao {
 
+	@Autowired
 	private JdbcTemplate jdbcTemplate;
-
-	public MemberDao(JdbcTemplate jdbcTemplate) {
-		super();
-		this.jdbcTemplate = jdbcTemplate;
-	}
 
 	public void create(Member member) {
 		jdbcTemplate.update("insert into member values (?,?,?,?,?)", 
